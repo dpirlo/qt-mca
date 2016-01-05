7f3e4df15e8efb35ce7462be4b0cacbde50fee88
Ariel Hernandez <ariel.h.estevenz@gmail.com>
2016-01-05 16:26:47 -0300

    Modificado: [apComunicacionMCA] Limpieza y modificación en el nombre de la clase de MSA->MCA.

--- apComunicacionMCA.cpp
+++ apComunicacionMCA.cpp
@@ -1,94 +1,14 @@
-#include "apComunicacionMSA.hpp"
+#include "apComunicacionMCA.hpp"
 
 using namespace ap;
 
-ComunicacionMSA::ComunicacionMSA()
+ComunicacionMCA::ComunicacionMCA()
 {
 
 }
 
-void ComunicacionMSA::Abort()
+void ComunicacionMCA::Abort()
 {
     Cancelar = true;
 }
 
-/*void ComunicacionMSA::Init(string port)
-{
-    bool ret = true;
-    pmtCount = 0;
-    InitVars();
-
-    SerialPortMCA.setPortName(port);
-    SerialPortMCA.TimeoutError = 2000;
-    SerialPortMCA.setBaudRate(921600,QSerialPort::AllDirections);
-
-    try
-    {
-      if (!SerialPortMCA.isOpen())
-         {
-         SetError("No se puede abrir el puerto en Init()");
-         ret = false;
-         }
-         else
-         {
-         ///Comando de Inicialización
-         ///Se envía el comando de incialización: "@0064010;\r"
-         ///y se debe recibir la respuesta "@0064020;\r"
-         SerialPortMCA.flush();
-         //SerialPortMCA.NewLine = Convert.ToString('\r'); //Defino el Enter para fin de recepcion //OJO con el retorno
-         SerialPortMCA.readAll();
-         SerialPortMCA.writeData("@0064010;",);//ver el maxSize
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-          string strTramaRecepcion;
-          ///Utilizando el metodo ReadLine() para recibir los datos, se finaliza
-          ///la recepción con el caracter de retorno de carro '\r'.
-          ///El mismo es quitado del string de recepción de manera autómatica,
-          ///por lo que no hay que tenerlo en cuenta.
-          strTramaRecepcion = SerialPortMCA.ReadLine();
-
-          if (U.Left(strTramaRecepcion, 5) == "@0064")//&& U.Right(strTramaRecepcion, 2) == "0<")
-             {
-              pmtCount = Convert.ToInt16(U.HexRawToDec(U.Mid(strTramaRecepcion, 6, 2))) - 1;
-              InitVars();
-              }
-         else
-             {
-              SetError("Respuesta inesperada: " + strTramaRecepcion + " en Init()");
-              ret = false;
-             }
-         }
-        ClosePort();
-        return ret;
-     }
-     catch (TimeoutException ToutExc)
-     {
-           SetError(ToutExc.Message + " en Init()");
-           SerialPortMCA.DiscardInBuffer();
-           return false;
-     }
-     catch (Exception exc)
-     {
-           SetError("La Placa MCA no responde en Init() : " + exc.Message);
-           return false;
-     }
-
-}
-
-bool ComunicacionMSA::InitVars()
-{
-     CantCanales = 1024;
-     SetMinMaxVentana(50, CantCanales - 1);
-     SetCanales(CantCanales);
-
-     pmtHV = new int[pmtCount + 1];
-     pmtTemp = new int[pmtCount + 1];
-     pmtTarget = new float[pmtCount + 1];
-     pmtPick = new int[pmtCount + 1];
-     Multicanal = new UInt32[pmtCount + 1][];
-     LastCanal = new UInt32[CantCanales];
-
-     for (int i = 0; i <= pmtCount; i++)
-          Multicanal[i] = new UInt32[CantCanales];
-
-     return true;
-}*/
--- apComunicacionMCA.hpp
+++ apComunicacionMCA.hpp
@@ -1,17 +1,15 @@
-#ifndef COMUNICACIONMSA_H
-#define COMUNICACIONMSA_H
+#ifndef COMUNICACIONMCA_H
+#define COMUNICACIONMCA_H
 
 #include <QtSerialPort>
 
 
 namespace ap {
 
-    class ComunicacionMSA
+    class ComunicacionMCA
     {
     public:
-        ComunicacionMSA();
-     //   void Init(string port);
-        bool InitVars();
+        ComunicacionMCA();
         void Abort();
 
     public:
@@ -33,43 +31,12 @@ namespace ap {
 
     private:
         bool Cancelar = false;                              //Cancela un procedimiento asincrónico
-/*        int CantCanales;
-        int MINCANAL;
-        int MAXCANAL;
-        const int HVMAXVALUE = 4096;
-        int LengthEncabezadoMCA;
-        int LengthDatosMCA;
-        int LevelOpen = 0;
-        QSerialPort SerialPortMCA;*/
+
 
     public:        
- /*       int pmtCount = 0;          // Cantidad de PMTs
-        const string CMD_HARDRESET = "@00690?";
-       // UInt32[][] Multicanal;
-       // UInt32[] LastCanal;
-       // double MulticanalTiempoMedicion = 0;
-       // int[] pmtTemp;
-       // int[] pmtHV;
-       // int[] pmtPick;
-       // float[] pmtTarget;
-        string OpcionesCalibracionTipo = "ctromasa";         //Opciones para la Calibración
-        int OpcionesCalibracionTiempo = 10000;
-        int OpcionesCalibracionPMT;
-        int OpcionesCalibracionTargetCh;
-        int OpcionesCalibracionCtroMasa;
-        int OpcionesCalibracionMesureTime;
-        bool CalibracionSuccess;
-        int OpcionesMedirEspectroTiempo = 20000;
-        int OpcionesMedirEspectroPMTini = 0;
-        int OpcionesMedirEspectroPMTfin = 0;
-        string OpcionesMedirEspectroDirOut = "";
-        bool MedicionEspectroSuccess;
-        string sErrorLevel = "";
-        string LastError;                                 //Variable pública para indicar el texto del último error
-        int ErrorLevel = 0;                                  //Indica el error con un número.*/
 
-    };
+      };
 
 }
 
-#endif // COMUNICACIONMSA_H
+#endif // COMUNICACIONMCA_H
