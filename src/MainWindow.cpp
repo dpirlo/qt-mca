6a9b8e3bf8cac6344bf9a63c236e763572631b83
Ariel Hernández <ariel.h.estevenz@ieee.org>
2017-05-29 16:04:53 -0300

    MOD: Número de cabezal en label (MainWindow)

--- src/MainWindow.cpp
+++ src/MainWindow.cpp
@@ -333,7 +333,7 @@ void MainWindow::receivedValuesMCA(long long time, int hv_pmt, int offset, int v
     {
         if (getCheckedHeads().length()==1)
         {
-            ui->label_title_output->setText("MCA Extended | Cabezal: " + getCheckedHeads().at(0));
+            ui->label_title_output->setText("MCA Extended | Cabezal: " + QString::number(getCheckedHeads().at(0)));
             ui->label_data_output->setText("| HV: "+QString::number(hv_pmt)+" | Varianza: "+QString::number(var)+" | Offset ADC: "+QString::number(offset)+" | Tiempo (mseg):"+QString::number(time/1000) + " |");
         }
         else
